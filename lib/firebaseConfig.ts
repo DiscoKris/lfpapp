86c19914a84898216a404bc13eef9e2c053f7ebd
DiscoKris <krislythgoe@me.com>
2025-09-28 07:06:54 -0700

    initial deploy

--- lib/firebaseConfig.ts
+++ lib/firebaseConfig.ts
@@ -1,19 +1,20 @@
+// lib/firebaseConfig.ts
 import { initializeApp, getApps, getApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAERRRFKrnd4Od85FqilwpPSdXthmwNacQ",
-  authDomain: "lfpapp-f8e73.firebaseapp.com",
-  projectId: "lfpapp-f8e73",
-  storageBucket: "lfpapp-f8e73.firebasestorage.app",
-  messagingSenderId: "1081308819125",
-  appId: "1:1081308819125:web:2529881a9e17ec9fe958fc"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 };
 
-const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
-export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
 export const storage = getStorage(app);
